6c94b31873d49abf17b38c965399c5f5bc62afdd
Linda Pacifique <71096252+IkireziPacifique@users.noreply.github.com>
2021-02-19 21:11:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-include 'CSS/index.html';
-include 'CSS/about.html';
-include 'CSS/store.html';
-include 'CSS/contact.html';
+include_one("CSS/index.html");
+include_one("CSS/about.html");
+include_one("CSS/store.html");
+include_one("CSS/contact.html");
 ?>
